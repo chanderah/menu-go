4e1bdfc2cd4e1fc09686e3adbcd12623807c59e0
chandraa01 <chandrachansa@gmail.com>
2023-08-01 18:00:43 +0700

    ..

--- middleware/middleware.go
+++ middleware/middleware.go
@@ -9,20 +9,20 @@ import (
 
 type bodyLogWriter struct {
 	gin.ResponseWriter
-	body *bytes.Buffer
+	buf *bytes.Buffer
 }
 
 func (w bodyLogWriter) Write(b []byte) (int, error) {
-	w.body.Write(b)
+	w.buf.Write(b)
 	return w.ResponseWriter.Write(b)
 }
 
 func GinLoggingMiddleware(c *gin.Context) {
-	blw := &bodyLogWriter{body: bytes.NewBufferString(""), ResponseWriter: c.Writer}
+	blw := &bodyLogWriter{ResponseWriter: c.Writer, buf: &bytes.Buffer{}}
 	c.Writer = blw
 	c.Next()
 
 	if c.Writer.Status() >= 400 {
-		fmt.Println("RESPONSE: " + blw.body.String())
+		fmt.Println("RESPONSE: " + blw.buf.String())
 	}
 }
