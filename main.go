27ec2d4c7b5fcbe4d3ef7c047f0cd0dcddf84491
Chandra Sukmagalih Arifin <chandrachansa@gmail.com>
2023-08-06 21:35:40 +0700

    Update main.go

--- main.go
+++ main.go
@@ -1,20 +1,32 @@
 package main
 
 import (
-	"github.com/chanderah/menu-go/response"
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	// serve()
-	port := "3001"
-	router := gin.New()
-	router.GET("/", func(c *gin.Context) {
-		response.OK(c, "Hi!")
-	})
+	e := echo.New()
 
-	router.Run(":" + port)
+	// Middleware
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
 
+	// Routes
+	e.GET("/", hello)
+
+	// Start server
+	e.Logger.Fatal(e.Start(":3001"))
+}
+
+// Handler
+func hello(c echo.Context) error {
+	return c.String(http.StatusOK, "Hellooo, World!")
 }
 
 func serve() {
