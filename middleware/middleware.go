3c12391be407a72f92c943231e690e8517779e92
Chandra Sukmagalih Arifin <chandrachansa@gmail.com>
2023-08-14 01:38:16 +0700

    ..

--- middleware/middleware.go
+++ middleware/middleware.go
@@ -29,8 +29,9 @@ func LoggingMiddleware(c *gin.Context) {
 
 func CorsMiddleware(c *gin.Context) {
 	c.Header("Access-Control-Allow-Origin", "*")
+	c.Header("Access-Control-Allow-Headers", "*")
 	c.Header("Access-Control-Allow-Credentials", "true")
-	c.Header("Access-Control-Allow-Headers", "User, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
+	// c.Header("Access-Control-Allow-Headers", "User, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
 	c.Header("Access-Control-Allow-Methods", "POST, GET, OPTIONS")
 	if c.Request.Method == "OPTIONS" {
 		c.AbortWithStatus(204)
