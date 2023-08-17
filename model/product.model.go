f7670d168e47ba0960cf5843a77a4c8144b34af6
Chandra Sukmagalih Arifin <chandrachansa@gmail.com>
2023-08-18 00:34:41 +0700

    ..

--- model/product.model.go
+++ model/product.model.go
@@ -23,6 +23,8 @@ type Product struct {
 	CategoryId  uint      `json:"categoryId,omitempty" gorm:"index:product_ix2"`
 	Name        string    `json:"name" gorm:"type:varchar(255);not null;index:product_ix1;index:product_ix2" binding:"required" `
 	Description string    `json:"description,omitempty" gorm:"type:varchar(255)"`
+	OptionType  string    `json:"optionType,omitempty" gorm:"type:varchar(100)"`
+	OptionValue string    `json:"optionValue,omitempty" gorm:"type:varchar(100)"`
 	Price       uint      `json:"price" gorm:"not null;index:product_ix1;index:product_ix2" binding:"required"`
 	Quantity    uint      `json:"quantity,omitempty"`
 	Status      string    `json:"status,omitempty" gorm:"type:varchar(100)"`
